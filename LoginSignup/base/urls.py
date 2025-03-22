73a06e1317cba8161d01b966b681929a06122b51
manmohanaeir1 <manmohanaeir001110@gmail.com>
2025-03-22 19:42:17 +0545

    login and signup fornt part

--- LoginSignup/base/urls.py
+++ LoginSignup/base/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path , include
+from .views import authView
 
 urlpatterns = [
-     
+    path("signup/" , authView, name="authView"), 
     path("accounts/", include("django.contrib.auth.urls")),
 ]
 
