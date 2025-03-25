4a7f68dc2e8763e28637cd04ffdce3551a66f3fd
manmohanaeir1 <manmohanaeir001110@gmail.com>
2025-03-25 12:41:00 +0545

    login and signup backend  part

--- LoginSignup/base/urls.py
+++ LoginSignup/base/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path , include
 from .views import authView
+from .views import home
 
 urlpatterns = [
+
+    path("", home, name="home"), 
     path("signup/" , authView, name="authView"), 
     path("accounts/", include("django.contrib.auth.urls")),
 ]
--- LoginSignup/base/views.py
+++ LoginSignup/base/views.py
@@ -1,8 +1,23 @@
-from django.shortcuts import render
-from  django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required  
 
+
+@login_required
+def home(request):
+    return render(request, "home.html", {})
+   
 # Create your views here.
+
+ 
+    
 
 def authView(request):
     form = UserCreationForm()
+    if request.method == "POST": 
+        form = UserCreationForm(request.POST or None)
+        if form.is_valid():
+            form.save()
+            return redirect("base:login")
     return render(request, "registration/signup.html", {"form": form})
+    
